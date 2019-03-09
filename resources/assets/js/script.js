44a34937bb581cbef3b4772ba3ddd9c9f2b5665c
takanashi66 <inoue@codecodeweb.com>
2019-03-09 15:41:54 +0900

    開発環境を変更

---
+++ resources/assets/js/script.js
@@ -0,0 +1,17 @@
+import $ from 'jquery'
+
+$(function(){
+    /* スムーズスクロール */
+    $('a[href^="#"]').on("click",function(){
+        var speed = 400; // ミリ秒
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+    })
+
+    $(".btn_none").on("click", function(){
+        return false;
+    })
+})
